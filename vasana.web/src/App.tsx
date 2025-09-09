47e0dd6170b8f5205f146f48af37f5d6cd6ff19d
basimchowdhury <basimchowdhury@gmail.com>
2025-09-09 00:05:29 -0400

    Simple init

--- vasana.web/src/App.tsx
+++ vasana.web/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [startRecording, setStartRecording] = useState<boolean>(false);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Track your habits
+                </p>
+                <button onClick={() => setStartRecording(true)}>
+                    Start
+                </button>
+            </header>
+            {startRecording && (<div className="modal-overlay">
+                <div className="modal-content">
+                    <h2>Modal Title</h2>
+                    <p>Modal content goes here</p>
+                    <button onClick={() => setStartRecording(false)}>Close</button>
+                </div>
+            </div>)}
+        </div>
+    );
 }
 
 export default App;
